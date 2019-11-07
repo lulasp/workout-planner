1b8fae016da5c1742870b9ab99c6bb654dd6c3b2
lula <lula__sp@hotmail.com>
2019-11-07 03:17:15 +0000

    first experiences with front end - app, landing, navbar & sidebar tests

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Fragment } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import Navbar from './components/layout/Navbar';
+import Sidebar from './components/layout/Sidebar';
+import Landing from './components/layout/Landing';
+
+
+/*THEME IMPORTS*/
+import './font-face.css';
+import './vendor/font-awesome-4.7/css/font-awesome.min.css';
+import './vendor/font-awesome-5/css/fontawesome-all.min.css';
+import './vendor/mdi-font/css/material-design-iconic-font.min.css';
+import './vendor/bootstrap-4.1/bootstrap.min.css';
+
+/*VENDOR CSS*/
+import './vendor/animsition/animsition.min.css';
+import './vendor/bootstrap-progressbar/bootstrap-progressbar-3.3.4.min.css';
+import './vendor/wow/animate.css';
+import './vendor/css-hamburgers/hamburgers.min.css';
+import './vendor/slick/slick.css';
+import './vendor/select2/select2.min.css';
+import './vendor/perfect-scrollbar/perfect-scrollbar.css';
+
+/*JS IMPORTS*/
+
+
+
+
+const App = () => (
+  <Fragment>
+    <Navbar />
+    <Sidebar />
+    <Landing />
+  </Fragment>
+);
 
 export default App;
